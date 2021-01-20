87e4368c6de529a000573dbf855daae52feffaca
enes abdüllatif yakacı <aenes1996@hotmail.com>
2021-01-20 03:57:40 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.HesapNo + " " + musteri.Açiklama + " " + "bilgilerine sahip müşteri sisteme eklendi.");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.HesapNo + " " + musteri.Açiklama);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.HesapNo + " " + musteri.Açiklama + " " + "bilgilerine sahip müşterinin kaydı sistemden kaldırıldı.");
+        }
+    }
+}
